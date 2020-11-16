258c77db506254f106730d44c6560ce8f9aa77f1
shriyans665 <73469664+shriyans665@users.noreply.github.com>
2020-11-16 18:36:54 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -47,7 +47,7 @@ function keyPressed(){
   if (keyCode === UP_ARROW && gameState === "play") {
     Matter.Body.applyForce(paper.body, paper.body.position, {
       x: 12,
-      y: -13
+      y: -15
     });
   }
 }
